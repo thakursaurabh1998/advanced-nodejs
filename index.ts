40e95a87f635432587edad5f428fcb1e4142ea92
Saurabh Thakur <mail@saurabhthakur.dev>
2020-09-21 01:45:15 +0530

    ADD: Simple chat application using net module

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import event from './cli-app-using-events';
+import net from './create-server/net';
 // import nodeAddon from './node-addon';
 
 const subdir = process.argv[2];
 const subDirMap: { [key: string]: Function } = {
   event,
+  net,
 };
 if (subDirMap[subdir]) {
   subDirMap[subdir]();
