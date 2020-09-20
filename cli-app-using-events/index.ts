e54941cc1abafd65d24ba67dea145491d9161563
Saurabh Thakur <mail@saurabhthakur.dev>
2020-09-21 02:12:23 +0530

    FIX: Readline starting on importing cli app

--- cli-app-using-events/index.ts
+++ cli-app-using-events/index.ts
@@ -1,24 +1,21 @@
 import { EventEmitter } from 'events';
 import server from './server';
-
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+import readline from 'readline';
 
 const client = new EventEmitter();
 
-rl.on('line', (input: string) => {
-  client.emit('input', input);
-});
-
 export default (): EventEmitter => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.on('line', (input: string) => {
+    client.emit('input', input);
+  });
   server(client).on('response', (resp) => {
     console.clear();
     process.stdout.write(resp);
-    process.stdout.write('\n\> ');
+    process.stdout.write('\n> ');
   });
   return client;
 };
